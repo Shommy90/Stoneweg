e0aedd63d734cbeeafbf9567170d766304e3ab8a
Milovan <marjanovic90@gmail.com>
2018-07-18 20:10:59 +0200

    homepage added (without mobile menu)

---
+++ js/main.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+
+  $('.slider').slick({
+      autoplay: true,
+      arrows: true
+  });
+
+
+});
+
+
+$(document).ready(function(){
+
+  $('.slider-designs').slick({
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 2000,
+      arrows: true
+  });
+
+
+});
